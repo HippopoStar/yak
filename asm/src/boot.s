3829ed3cb333b00d158d33c6e85dfcac3e904455
HippopoStar <leocabanes@wanadoo.fr>
2025-03-17 03:27:20 +0100

    Fix x86 assembly "hello_word"

--- asm/src/boot.s
+++ asm/src/boot.s
@@ -16,10 +16,12 @@ put_str:                             ; → Max string length = 80x25 bytes (VGA 
 	mov ecx, str_len                 ; Initialize count
 	jecxz put_str_end                ; Jump if count is 0
 	dec ecx                          ; Decrement by 1
+	jecxz put_str_first_char         ; Jump if count is 0 (avoid 2³² loop iterations)
 put_str_loop:
 	mov byte dl, [str + ecx]         ; Move data to register from memory
 	mov byte [0xb8000 + ecx * 2], dl ; Move data to memory from register
 	loop put_str_loop                ; Decrement count & jump if count ≠ 0
+put_str_first_char:
 	mov byte dl, [str]
 	mov byte [0xb8000], dl
 put_str_end:
